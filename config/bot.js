53a8f6ddf1cf3f8c01ff52cb02338609e8c0837d
Nick06888 <nikmorinelli@tiscali.it>
2021-03-09 18:09:10 +0100

    bot

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODE0NjA0NjIwNjYzNDg4NTUz.YDgRrA.fRT3xt-OTijBpGM4Z_1CFoRVHp4',
+        prefix: '*',
+        activity: 'By Nick134#1479',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
